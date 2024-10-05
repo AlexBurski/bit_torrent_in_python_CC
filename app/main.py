0036a65c65c629d9a6cf092944d5c653618c52d4
Alexander Burski <bssb.sbbs@mail.ru>
2024-10-05 15:25:11 -0400

    Parse torrent file

--- app/main.py
+++ app/main.py
@@ -5,32 +5,58 @@ import bencodepy
 
 # import requests - available if you need it!
 
+
 def decode_bencode(bencoded_value):
-    bc = bencodepy.Bencode(encoding='utf-8')
+    bc = bencodepy.Bencode(encoding=None)
     decoded_value = bc.decode(bencoded_value)
     return decoded_value
 
 
+def bytes_to_str(data):
+    if isinstance(data, bytes):
+        try:
+            return data.decode("utf-8")
+        except UnicodeDecodeError:
+            return repr(data)
+
+    elif isinstance(data, dict):
+        return {bytes_to_str(k): bytes_to_str(v) for k, v in data.items()}
+
+    elif isinstance(data, list):
+        return [bytes_to_str(v) for v in data]
+
+    else:
+        return data
+
+
 def main():
     command = sys.argv[1]
-
-    # print("Logs from your program will appear here!")
-
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
-
+        decoded_value = decode_bencode(bencoded_value)
+        decoded_str = bytes_to_str(decoded_value)
+        print(json.dumps(decoded_str))
         # json.dumps() can't handle bytes, but bencoded "strings" need to be
         # bytestrings since they might contain non utf-8 characters.
         #
         # Let's convert them to strings for printing to the console.
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
-
-            raise TypeError(f"Type not serializable: {type(data)}")
 
         # Uncomment this block to pass the first stage
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+
+    elif command == "info":
+        file_to_read = sys.argv[2]
+        with open(file_to_read, "rb") as f:
+            bencoded_value = f.read()
+            # print(bencoded_value)
+            decoded_dict = decode_bencode(bencoded_value)
+
+            tracker_url = decoded_dict.get(b"announce").decode("utf-8")
+            file_length = decoded_dict.get(b"info", {}).get(b"length")
+
+            print(f"Tracker URL: {tracker_url}")
+            print(f"Length: {file_length}")
+
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
